cc5637ec89adf951078bbbe744c984eea43ba057
SebastianBran <sebastianbranzapata@gmail.com>
2021-12-30 12:22:43 -0500

    initial commit

---
+++ web-services/src/main/java/com/example/webservicepokeapi/WebServicePokeApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.webservicepokeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicePokeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicePokeApiApplication.class, args);
+    }
+
+}
---
+++ web-services/src/test/java/com/example/webservicepokeapi/WebServicePokeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webservicepokeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServicePokeApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
