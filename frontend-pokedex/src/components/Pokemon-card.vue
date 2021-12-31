ca0e16e85a3aadbed0359646145a3f8c911de638
SebastianBran <sebastianbranzapata@gmail.com>
2021-12-31 10:35:06 -0500

    feat: components added

---
+++ frontend-pokedex/src/components/Pokemon-card.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-card :to="`pokemons/information/${name}`">
+    <v-img :src="imagePath"></v-img>
+    <v-card-title>{{ name }}</v-card-title>
+    <v-card-subtitle class="text-start">Id: {{ id }}</v-card-subtitle>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Pokemon-card",
+  props: {
+    imagePath: String,
+    name: String,
+    id: Number,
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
